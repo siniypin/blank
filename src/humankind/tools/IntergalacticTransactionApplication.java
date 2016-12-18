97cc68cc76d670bc03c3b1ce2dd68ec65fc3e609
Boris Tveritnev <boris.tveritnev@xing.com>
2016-12-18 01:52:36 +0100

    handling file stream errors

--- src/humankind/tools/IntergalacticTransactionApplication.java
+++ src/humankind/tools/IntergalacticTransactionApplication.java
@@ -6,18 +6,20 @@ public class IntergalacticTransactionApplication {
 	public static void main(String[] args) throws FileNotFoundException {
 		InputStream in = System.in;
 		OutputStream out = System.out;
+		try {
 		if (args.length > 0) {
-			in = new FileInputStream(args[0]);// TODO: FileNotFoundException
-												// SecurityException
+			in = new FileInputStream(args[0]);
 		}
 		if (args.length > 1) {
-			out = new FileOutputStream(args[1]);// TODO: FileNotFoundException
-												// SecurityException
+			out = new FileOutputStream(args[1]);
 		}
 
 		TransactionContext context = new TransactionContext(new BufferedReader(new InputStreamReader(in)),
 				new PrintWriter(new BufferedWriter(new OutputStreamWriter(out))));
 		TransactionParser parser = new TransactionParser(context);
 		context.processInput(parser);
+		} catch (FileNotFoundException | SecurityException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
